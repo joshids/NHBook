1223d7724e13241dae54f12dac74fa8dc3b5e43a
Darshan Joshi <joshids.work@gmail.com>
2015-04-22 04:59:30 +0530

    Update 1

---
+++ Chapter_1/Final/Chapter1/Chapter1.Test/BasicTests.cs
@@ -0,0 +1,33 @@
+﻿using Chapter1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Chapter1.Test
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void CanBuildSessionFactory()
+        {
+            var cfg = new NHibernate.Cfg.Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Student).Assembly);
+            cfg.BuildMappings();
+            var sessionFactory = cfg.BuildSessionFactory();
+        }
+
+        [TestMethod]
+        public void CanReadStudentRecord()
+        {
+            var cfg = new NHibernate.Cfg.Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Student).Assembly);
+            cfg.BuildMappings();
+            var sessionFactory = cfg.BuildSessionFactory();
+            var student = sessionFactory.OpenSession().Get<Student>(new Guid("75079B2F-C837-40D4-8F75-5C75400E6C1E"));
+
+            Assert.IsNotNull(student);
+        }
+    }
+}
---
+++ Chapter_1/Final/Chapter1/Chapter1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    public class Student
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+    }
+}
